410334eab930e542f647220875058a4839142a35
user01 <user01@user01noMacBook-Air.local>
2022-08-19 07:42:31 +0900

    test

---
+++ src/graphql/queries.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getEvent = /* GraphQL */ `
+  query GetEvent($id: ID!) {
+    getEvent(id: $id) {
+      id
+      name
+      where
+      when
+      description
+      comments {
+        nextToken
+      }
+    }
+  }
+`;
+export const listEvents = /* GraphQL */ `
+  query ListEvents(
+    $filter: TableEventFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listEvents(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        where
+        when
+        description
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const subscribeToEventComments = /* GraphQL */ `
+  subscription SubscribeToEventComments($eventId: String!) {
+    subscribeToEventComments(eventId: $eventId) {
+      eventId
+      commentId
+      content
+      createdAt
+    }
+  }
+`;
